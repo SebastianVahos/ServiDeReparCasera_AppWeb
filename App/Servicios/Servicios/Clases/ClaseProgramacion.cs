c5c9afa178940df18cc9bc3cfbc0f9ae27f88345
Sonistwo <159084179+Sonistwo@users.noreply.github.com>
2025-05-25 17:52:06 -0500

    Creacion de servicio y controlador para Programacion. Faltan metodos de reglas de negocio

---
+++ App/Servicios/Servicios/Clases/ClaseProgramacion.cs
@@ -0,0 +1,60 @@
+﻿using Servicios.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Servicios.Clases
+{
+    public class ClaseProgramacion
+    {
+
+        private readonly ServiDeReparCaserosEntities _context;
+
+        public ClaseProgramacion()
+        {
+            _context = new ServiDeReparCaserosEntities();
+        }
+
+        public List<Programacion> ObtenerProgramaciones()
+        {
+            return _context.Programacions.ToList();
+        }
+
+        public Programacion ObtenerProgramacionPorID(int id)
+        {
+            return _context.Programacions.FirstOrDefault(p => p.IdProgramacion == id);
+        }
+
+        public void AgregarProgramacion(Programacion programacion)
+        {
+            _context.Programacions.Add(programacion);
+            _context.SaveChanges();
+        }
+
+        public void ActualizarProgramacion(Programacion programacion)
+        {
+            var servicioExistente = _context.Programacions.FirstOrDefault(p => p.IdProgramacion == programacion.IdProgramacion);
+            if (servicioExistente != null)
+            {
+                servicioExistente.Fecha = programacion.Fecha;
+                servicioExistente.Hora = programacion.Hora;
+                servicioExistente.Direccion = programacion.Direccion;
+                servicioExistente.Estado = programacion.Estado;
+                servicioExistente.Documento = programacion.Documento;
+                servicioExistente.CodigoServicio = programacion.CodigoServicio;
+                _context.SaveChanges();
+            }
+        }
+        public void EliminarProgramacion(int id)
+        {
+            var servicio = _context.Programacions.FirstOrDefault(p => p.IdProgramacion == id);
+            if (servicio != null)
+            {
+                _context.Programacions.Remove(servicio);
+                _context.SaveChanges();
+            }
+        }
+
+    }
+}
---
+++ App/Servicios/Servicios/Controllers/ProgramacionController.cs
@@ -0,0 +1,66 @@
+﻿using Servicios.Clases;
+using Servicios.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace Servicios.Controllers
+{
+    [RoutePrefix("api/programaciones")]
+    public class ProgramacionController : ApiController
+    {
+
+        ClaseProgramacion ClaseProgramacion = new ClaseProgramacion();
+
+        [HttpGet]
+        [Route("consultarTodos")]
+        public List<Programacion> ObtenerServicios()
+        {
+            return ClaseProgramacion.ObtenerProgramaciones();
+        }
+
+        [HttpGet]
+        [Route("consultar")]
+        public Programacion ObtenerServicioPorId(int id)
+        {
+            return ClaseProgramacion.ObtenerProgramacionPorID(id);
+        }
+
+        [HttpPost]
+        [Route("agregar")]
+        public IHttpActionResult AgregarProgramacion([FromBody] Programacion programacion)
+        {
+            if (programacion == null)
+            {
+                return BadRequest("La programación no puede ser nula.");
+            }
+
+            ClaseProgramacion.AgregarProgramacion(programacion);
+            return Ok("Programacion agregada correctamente.");
+        }
+
+        [HttpPut]
+        [Route("actualizar")]
+        public IHttpActionResult ActualizarProgramacion([FromBody] Programacion programacion)
+        {
+            if (programacion == null)
+            {
+                return BadRequest("La programación no puede ser nula.");
+            }
+
+            ClaseProgramacion.ActualizarProgramacion(programacion);
+            return Ok("Programación actualizada correctamente.");
+        }
+
+        [HttpDelete]
+        [Route("eliminar")]
+        public IHttpActionResult EliminarProgramacion(int id)
+        {
+            ClaseProgramacion.EliminarProgramacion(id);
+            return Ok("Programación eliminada correctamente.");
+        }
+
+    }
+}
