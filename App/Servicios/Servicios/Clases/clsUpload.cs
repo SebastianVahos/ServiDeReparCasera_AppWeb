87278f0e1d599f8ad5bdf7ebedae14bbbabfbdd0
CarolinaHenao21 <carolinahenao329281@correo.itm.edu.co>
2025-05-31 18:11:17 -0500

    Creacion de las clases y controllers de Equipo y Archivos del equipo(imagenes)

---
+++ App/Servicios/Servicios/Clases/clsUpload.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Servicios.Clases
+{
+    public class clsUpload
+    {
+        public string Datos { get; set; }
+        public string Proceso { get; set; }
+        public HttpRequestMessage request { get; set; }
+        private List<string> Archivos;
+
+        public async Task<HttpResponseMessage> GrabarArchivo(bool Actualizar, bool Eliminar)
+        {
+            if (!request.Content.IsMimeMultipartContent())
+            {
+                return request.CreateErrorResponse(System.Net.HttpStatusCode.InternalServerError, "No se envió un archivo para procesar");
+            }
+            string root = HttpContext.Current.Server.MapPath("~/Archivos");
+            var provider = new MultipartFormDataStreamProvider(root);
+            try
+            {
+                bool Existe = false;
+                await request.Content.ReadAsMultipartAsync(provider);
+                if (provider.FileData.Count > 0)
+                {
+                    Archivos = new List<string>();
+                    foreach (MultipartFileData file in provider.FileData)
+                    {
+                        string fileName = file.Headers.ContentDisposition.FileName;
+                        if (fileName.StartsWith("\"") && fileName.EndsWith("\""))
+                        {
+                            fileName = fileName.Trim('"');
+                        }
+                        if (fileName.Contains(@"/") || fileName.Contains(@"\"))
+                        {
+                            fileName = Path.GetFileName(fileName);
+                        }
+                        if (File.Exists(Path.Combine(root, fileName)))//validar si el nombre del archivo existe enla bd
+                        {
+                            if (Actualizar)
+                            {
+                                //El archivo ya existe en el server, no se va a cargar, se va eliminar el temporal y se devuelvee un error
+                                File.Delete(Path.Combine(root, fileName));
+                                //actualiza el nombre del primer archivo
+                                File.Move(file.LocalFileName, Path.Combine(root, fileName));
+                                return request.CreateResponse(System.Net.HttpStatusCode.OK, "Se actualizó la imagen");
+                            }
+                            else if (Eliminar)
+                            {
+                                //Se genera el proceso de gestion en la base de datos
+                                string RptaBD = ProcesarBD(Eliminar);
+                                File.Delete(Path.Combine(root, fileName));//Elimina el archivo 
+                                File.Delete(Path.Combine(root, file.LocalFileName));//elimina el archivo temporal
+                                return request.CreateResponse(System.Net.HttpStatusCode.OK, "Se elimino la imagen, " + RptaBD);
+                            }
+                            else
+                            {
+                                //El archivo ya existe en el server, no se va a cargar, se va eliminar el temporal y se devuelvee un error
+                                File.Delete(Path.Combine(root, file.LocalFileName));
+                                Existe = true;
+                            }
+                            //return request.CreateErrorResponse(System.Net.HttpStatusCode.InternalServerError, "El archivo ya existe");
+                        }
+                        else
+                        {
+                            if (!Actualizar)
+                            {
+                                Existe = false;
+                                //Agrego en una lista el nombre de los archivos que se cargaron
+                                Archivos.Add(fileName);
+                                //renombra el archivo temporal
+                                File.Move(file.LocalFileName, Path.Combine(root, fileName));
+                            }
+                            else
+                            {
+                                return request.CreateErrorResponse(System.Net.HttpStatusCode.InternalServerError, "El archivo no existe, se debe agregar");
+                            }
+                        }
+                    }
+                    if (!Existe)
+                    {
+                        //se genera el proceso de gestion en la bd
+                        string RptaBD = ProcesarBD(Eliminar);
+                        //termina el ciclo, responde que se cargó el archivo correctamente
+                        return request.CreateResponse(System.Net.HttpStatusCode.OK, "Se cargaron los archivos en el servidor");
+                    }
+                    else
+                    {
+                        return request.CreateErrorResponse(System.Net.HttpStatusCode.InternalServerError, "El archivo ya existe");
+                    }
+                }
+                else
+                {
+                    return request.CreateErrorResponse(System.Net.HttpStatusCode.Conflict, "No se envió un archivo para procesar");
+                }                
+            }
+            catch (Exception ex)
+            {
+                return request.CreateErrorResponse(System.Net.HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+        public HttpResponseMessage DescargarArchivo(string Imagen)
+        {
+            try
+            {
+                string Ruta = HttpContext.Current.Server.MapPath("~/Archivos");
+                string Archivo = Path.Combine(Ruta, Imagen);
+                if (File.Exists(Archivo))
+                {
+                    HttpResponseMessage response = new HttpResponseMessage(System.Net.HttpStatusCode.OK);
+                    var stream = new FileStream(Archivo, FileMode.Open);
+                    response.Content = new StreamContent(stream);
+                    response.Content.Headers.ContentDisposition = new System.Net.Http.Headers.ContentDispositionHeaderValue("attachment");
+                    response.Content.Headers.ContentDisposition.FileName = Imagen;
+                    response.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/octet-stream");
+                    return response;
+                }
+                else
+                {
+                    return request.CreateErrorResponse(System.Net.HttpStatusCode.NoContent, "No se encontró el archivo");
+                }
+            }
+            catch (Exception ex)
+            {
+                return request.CreateErrorResponse(System.Net.HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+        private string ProcesarBD(bool Eliminar)
+        {
+            switch (Proceso.ToUpper())
+            {
+                case "EQUIPO":
+                    if (!Eliminar)
+
+                    {
+                        clsEquipo equipo = new clsEquipo();
+                        return equipo.GrabarImagenEquipo(Convert.ToInt32(Datos), Archivos);
+                    }
+                    else
+                    {
+                        clsEquipo equipo = new clsEquipo();
+                        return equipo.EliminarImagenEquipo(Convert.ToInt32(Datos));
+                    }                  
+                default:
+                    return "No se ha definido el proceso en la base de datos";
+            }
+        }
+
+    }
+}
