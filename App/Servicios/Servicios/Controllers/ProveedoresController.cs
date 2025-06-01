bf55f9649700a76cbc8dff2136e6283681cc5f36
CarolinaHenao21 <carolinahenao329281@correo.itm.edu.co>
2025-05-31 19:39:24 -0500

    Creación de proveedor y su controller, adicional se implementó EquipoProveedor

--- App/Servicios/Servicios/Controllers/ProveedoresController.cs
+++ App/Servicios/Servicios/Controllers/ProveedoresController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Servicios.Clases;
+using Servicios.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,7 +8,51 @@ using System.Web.Http;
 
 namespace Servicios.Controllers
 {
+    [RoutePrefix("api/Proveedores")]
+    [Authorize]
     public class ProveedoresController : ApiController
     {
+        [HttpGet]
+        [Route("ConsultarTodos")]
+        public List<Proveedor> ConsultarTodos()
+        {
+            clsProveedor proveedor = new clsProveedor();
+            return proveedor.ConsultarTodos();
+        }
+
+        [HttpGet]
+        [Route("Consultar")]
+        public Proveedor Consultar(string idProveedor)
+        {
+            clsProveedor proveedor = new clsProveedor();
+            return proveedor.Consultar(idProveedor);
+        }
+
+        [HttpPost]
+        [Route("Insertar")]
+        public string Insertar([FromBody] Proveedor proveedor, int codEquipo, decimal vlrUnitario, DateTime fechaCoti, DateTime fechaVali)
+        {
+            clsProveedor Proveedor = new clsProveedor();
+            Proveedor.proveedor = proveedor;
+            return Proveedor.Insertar(codEquipo, vlrUnitario, fechaCoti, fechaVali);
+        }
+
+        [HttpPut]
+        [Route("Actualizar")]
+        public string Actualizar([FromBody] Proveedor proveedor)
+        {
+            clsProveedor Proveedor = new clsProveedor();
+            Proveedor.proveedor = proveedor;
+            return Proveedor.Actualizar();
+        }
+
+        [HttpDelete]
+        [Route("EliminarXCodigo")]
+        public string EliminarXCodigo(string idProveedor)
+        {
+            clsProveedor Proveedor = new clsProveedor();
+            return Proveedor.Eliminar(idProveedor);
+        }
+
     }
 }
