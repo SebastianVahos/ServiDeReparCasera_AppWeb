7808c63077d1bdc5206138e72567857571291cf8
ErikaM2006 <gracianomarcela01@gmail.com>
2025-06-02 18:25:53 -0500

    Servicios empleados y facturas

---
+++ App/Servicios/Servicios/Clases/clsFactura.cs
@@ -0,0 +1,102 @@
+﻿using Servicios.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+
+namespace Servicios.Clases
+{
+    public class clsFactura
+    {
+        private ServiDeReparCaserosEntities1 dbServiDeReparCaseros = new ServiDeReparCaserosEntities1();
+
+        public Factura factura { get; set; }
+
+        public string Insertar()
+        {
+            try
+            {
+                dbServiDeReparCaseros.Facturas.Add(factura);
+                dbServiDeReparCaseros.SaveChanges();
+                return "Factura insertada correctamente";
+            }
+            catch (Exception ex)
+            {
+                return "Error al insertar la factura: " + ex.Message;
+            }
+        }
+
+        public string Actualizar()
+        {
+            try
+            {
+                Factura fact = Consultar(factura.Numero);
+                if (fact == null)
+                {
+                    return "La factura no existe, por lo tanto no se puede actualizar";
+                }
+
+                dbServiDeReparCaseros.Facturas.AddOrUpdate(factura);
+                dbServiDeReparCaseros.SaveChanges();
+                return "Factura actualizada correctamente";
+            }
+            catch (Exception ex)
+            {
+                return "No se pudo actualizar la factura: " + ex.Message;
+            }
+        }
+
+        public List<Factura> ConsultarTodas()
+        {
+            return dbServiDeReparCaseros.Facturas
+                .OrderBy(f => f.Fecha)
+                .ToList();
+        }
+
+        public Factura Consultar(int Numero)
+        {
+            return dbServiDeReparCaseros.Facturas
+                .FirstOrDefault(f => f.Numero == Numero);
+        }
+
+        public string Eliminar()
+        {
+            try
+            {
+                Factura fact = Consultar(factura.Numero);
+                if (fact == null)
+                {
+                    return "La factura no existe, por lo tanto no se puede eliminar";
+                }
+
+                dbServiDeReparCaseros.Facturas.Remove(fact);
+                dbServiDeReparCaseros.SaveChanges();
+                return "Factura eliminada correctamente";
+            }
+            catch (Exception ex)
+            {
+                return "No se pudo eliminar la factura: " + ex.Message;
+            }
+        }
+
+        public string EliminarXNumero(int Numero)
+        {
+            try
+            {
+                Factura fact = Consultar(Numero);
+                if (fact == null)
+                {
+                    return "La factura no existe, por lo tanto no se puede eliminar";
+                }
+
+                dbServiDeReparCaseros.Facturas.Remove(fact);
+                dbServiDeReparCaseros.SaveChanges();
+                return "Factura eliminada correctamente";
+            }
+            catch (Exception ex)
+            {
+                return "No se pudo eliminar la factura: " + ex.Message;
+            }
+        }
+    }
+}
---
+++ App/Servicios/Servicios/Controllers/FacturasController.cs
@@ -0,0 +1,71 @@
+﻿using Servicios.Clases;
+using Servicios.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace Servicios.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    [RoutePrefix("api/Facturas")]
+    [Authorize]
+    public class FacturasController : ApiController
+    {
+        [HttpGet] //Es el servicio que se va a exponer: GET, POST, PUT, DELETE
+        [Route("ConsultarTodos")] //Es el nombre de la funcionalidad que se va a ejecutar
+        public List<Factura> ConsultarTodas()
+        {
+            //Se crea una instancia de la clase clsEmpleado
+            clsFactura Factura = new clsFactura();
+            //Se invoca el método ConsultarTodos() de la clase clsEmpleado
+            return Factura.ConsultarTodas();
+        }
+
+        [HttpGet]
+        [Route("ConsultarXNumero")]
+        public Factura ConsultarXNumero(int Numero)
+        {
+            clsFactura factura = new clsFactura();
+            return factura.Consultar(Numero);
+        }
+
+        [HttpPost]
+        [Route("Insertar")]
+        public string Insertar([FromBody] Factura facturaInput)
+        {
+            clsFactura factura = new clsFactura();
+            factura.factura = facturaInput;
+            return factura.Insertar();
+        }
+
+        [HttpPut]
+        [Route("Actualizar")]
+        public string Actualizar([FromBody] Factura facturaInput)
+        {
+            clsFactura factura = new clsFactura();
+            factura.factura = facturaInput;
+            return factura.Actualizar();
+        }
+
+        [HttpDelete]
+        [Route("Eliminar")]
+        public string Eliminar([FromBody] Factura facturaInput)
+        {
+            clsFactura factura = new clsFactura();
+            factura.factura = facturaInput;
+            return factura.Eliminar();
+        }
+
+        [HttpDelete]
+        [Route("EliminarXNumero")]
+        public string EliminarXNumero(int Numero)
+        {
+            clsFactura factura = new clsFactura();
+            return factura.EliminarXNumero(Numero);
+        }
+    }
+}
