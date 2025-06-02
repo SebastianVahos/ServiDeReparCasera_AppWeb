03d34b94b74abeecdb65860f6503ea0a9bc8ed8f
SebastianVahos <vahossebas@gmail.com>
2025-06-01 21:38:18 -0500

    new file

---
+++ App/Servicios/Servicios/Clases/clsLogin.cs
@@ -0,0 +1,93 @@
+﻿using Servicios.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Servicios.Clases
+{
+    public class clsLogin
+    {
+        public clsLogin()
+        {
+            loginRespuesta = new LoginRespuesta();//Cuando me construyan el objeto, creo la respuesta para poderla utilizar
+        }
+        private ServiDeReparCaserosEntities DBServi = new ServiDeReparCaserosEntities();
+        public Login login { get; set; }
+        public LoginRespuesta loginRespuesta { get; set; }
+        public bool ValidarUsuario()
+        {
+            try
+            {
+                clsCypher cifrar = new clsCypher();//Creamos un objeto cifrar, para poder cifrar la clave
+                Usuario usuario = DBServi.Usuarios.FirstOrDefault(u => u.UserName == login.Usuario);//lea el usuario
+                if (usuario == null)
+                {
+                    loginRespuesta.Autenticado = false;
+                    loginRespuesta.Mensaje = "Usuario no existe";
+                    return false;
+                }
+                byte[] arrBytesSalt = Convert.FromBase64String(usuario.Salt);//obtiene el salt y lo convierte a Bytes
+                string ClaveCifrada = cifrar.HashPassword(login.Clave, arrBytesSalt);//tiene una clave plana(login.clave) y encripta la clave con el arreglo de bytes que tiene
+                login.Clave = ClaveCifrada;//y lo asigna a esa clave y ya esa clave esta encriptada
+                return true;
+            }
+            catch (Exception ex)
+            {
+                loginRespuesta.Autenticado = false;
+                loginRespuesta.Mensaje = ex.Message;
+                return false;
+            }
+        }
+        private bool ValidarClave()
+        {
+            try
+            {                                                                                       //ya esta clave esta encriptada(u.clave) y compara la encripcion con encripcion que esta en la BD
+                Usuario usuario = DBServi.Usuarios.FirstOrDefault(u => u.UserName == login.Usuario && u.Clave == login.Clave);
+                if (usuario == null)
+                {
+                    loginRespuesta.Autenticado = false;
+                    loginRespuesta.Mensaje = "La clave no coincide";
+                    return false;
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                loginRespuesta.Autenticado = false;
+                loginRespuesta.Mensaje = ex.Message;
+                return false;
+            }
+        }
+        public IQueryable<LoginRespuesta> Ingresar()
+        {
+            if (ValidarUsuario() && ValidarClave())
+            {
+                //Se genera el token
+                string Token = TokenGenerator.GenerateTokenJwt(login.Usuario);
+                return from U in DBServi.Set<Usuario>()
+                       join UP in DBServi.Set<PerfilUsuario>()
+                       on U.IdUsuario equals UP.IdUsuario
+                       join P in DBServi.Set<Perfil>()
+                       on UP.IdPerfil equals P.IdPerfil
+                       where U.UserName == login.Usuario &&
+                               U.Clave == login.Clave
+                       select new LoginRespuesta
+                       {
+                           Usuario = U.UserName,
+                           Autenticado = true,
+                           Perfil = P.Nombre,
+                           PaginaInicio = P.PaginaNavegar,
+                           Token = Token,
+                           Mensaje = ""
+                       };
+            }
+            else
+            {
+                List<LoginRespuesta> listRpta = new List<LoginRespuesta>();
+                listRpta.Add(loginRespuesta);
+                return listRpta.AsQueryable();
+            }
+        }
+    }
+}
---
+++ App/Servicios/Servicios/Clases/clsProveedor.cs
@@ -0,0 +1,93 @@
+﻿using Servicios.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+
+namespace Servicios.Clases
+{
+    public class clsProveedor
+    {
+        private ServiDeReparCaserosEntities DBServi = new ServiDeReparCaserosEntities();
+        public Proveedor proveedor { get; set; }
+
+        public string Insertar(int codEquipo, decimal vlrUnitario, DateTime fechaCoti, DateTime fechaVali)
+        {
+            try
+            {
+                DBServi.Proveedors.Add(proveedor);
+                DBServi.SaveChanges();
+                //Se debe grabar el equipo proveedor 
+                EquipoProveedor equipoProveedor = new EquipoProveedor();
+                equipoProveedor.IdProveedor = proveedor.IdProveedor;
+                equipoProveedor.CodigoEquipo = codEquipo;
+                equipoProveedor.ValorUnitario = vlrUnitario;
+                equipoProveedor.FechaCotizacion = fechaCoti;
+                equipoProveedor.FechaValidez = fechaVali;
+                DBServi.EquipoProveedors.Add(equipoProveedor);
+                DBServi.SaveChanges();
+                return "Proveedor insertado correctamente";
+            }
+            catch (Exception ex)
+            {
+                return "Error al insertar el proveedor: " + ex.Message;
+            }
+        }
+
+        public string Actualizar()
+        {
+            try
+            {
+                Proveedor prov = Consultar(proveedor.IdProveedor);
+                if (prov == null)
+                {
+                    return "El proveedor con el código ingresado no existe, no se puede actualizar";
+                }
+                DBServi.Proveedors.AddOrUpdate(proveedor);
+                DBServi.SaveChanges();
+                return "Se actualizó el proveedor correctamente";
+            }
+            catch (Exception ex)
+            {
+                return "No se pudo actualizar el proveedor: " + ex.Message;
+            }
+        }
+
+        public List<Proveedor> ConsultarTodos()
+        {
+            return DBServi.Proveedors.ToList();
+        }
+        public Proveedor Consultar(string idProveedor)
+        {
+            return DBServi.Proveedors.FirstOrDefault(e => e.IdProveedor == idProveedor);
+        }
+        public EquipoProveedor ConsultarDos(string idProveedor)
+        {
+            return DBServi.EquipoProveedors.FirstOrDefault(e => e.IdProveedor == idProveedor);
+        }
+
+        public string Eliminar(string idProveedor)
+        {
+            try
+            {
+                EquipoProveedor prov = ConsultarDos(idProveedor);
+                if (prov == null)
+                {
+                    return "El proveedor con el codigo ingresado no existe, por lo tanto no se puede eliminar";
+                }
+                DBServi.EquipoProveedors.Remove(prov);
+                DBServi.SaveChanges();
+                Proveedor prove = Consultar(idProveedor);
+                DBServi.Proveedors.Remove(prove);
+                DBServi.SaveChanges();
+                return "Se eliminó el proveedor correctamente";
+            }
+            catch (Exception ex)
+            {
+                return "No se pudo eliminar el proveedor: " + ex.Message;
+            }
+        }
+
+    }
+}
