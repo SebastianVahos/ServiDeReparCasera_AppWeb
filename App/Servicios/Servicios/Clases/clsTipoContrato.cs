27d808b8e5ada4bbad21ee1f1ff1a7de303374ed
ErikaM2006 <gracianomarcela01@gmail.com>
2025-06-03 12:27:48 -0500

    Se hace el servicio de Tipo de contrato

---
+++ App/Servicios/Servicios/Clases/clsTipoContrato.cs
@@ -0,0 +1,69 @@
+﻿using Servicios.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Servicios.Clases
+{
+    public class clsTipoContrato
+    {
+        ServiDeReparCaserosEntities DBServi = new ServiDeReparCaserosEntities();
+        public TipoContrato tipoContrato { get; set; }
+
+        public List<TipoContrato> ConsultarTodos()
+        {
+            return DBServi.TipoContratoes
+                .OrderBy(e => e.Nombre)
+                .ToList();
+        }
+
+        public TipoContrato Consultar(int Documento)
+        {
+            return DBServi.TipoContratoes
+                .FirstOrDefault(e => e.IdTipoContrato == Documento);
+        }
+
+        public string Insertar()
+        {
+            DBServi.TipoContratoes.Add(tipoContrato);
+            DBServi.SaveChanges();
+            return "Se insertó el Tipo de Contrato: " + tipoContrato.IdTipoContrato;
+        }
+        public string Eliminar()
+        {
+            TipoContrato _tipoContrato = DBServi.TipoContratoes.FirstOrDefault(t => t.IdTipoContrato == tipoContrato.IdTipoContrato);
+            DBServi.TipoContratoes.Remove(_tipoContrato);
+            DBServi.SaveChanges();
+            return "Se eliminó el contrato con Id : " + tipoContrato.IdTipoContrato;
+        }
+        public string Actualizar()
+        {
+            TipoContrato _tipoContrato = DBServi.TipoContratoes.FirstOrDefault(t => t.IdTipoContrato == tipoContrato.IdTipoContrato);
+            _tipoContrato.IdTipoContrato = tipoContrato.IdTipoContrato;
+            _tipoContrato.Nombre = tipoContrato.Nombre;
+
+            DBServi.SaveChanges();
+            return "Se actualizó el Tipo Contrato con Id: " + tipoContrato.IdTipoContrato;
+        }
+        public string EliminarXIdTipoContrato(int documento)
+        {
+            try
+            {
+                TipoContrato empl = Consultar(documento);
+                if (empl == null)
+                {
+                    return "El Tipo Contrato con el Id ingresado no existe, por lo tanto no se puede eliminar";
+                }
+
+                DBServi.TipoContratoes.Remove(empl);
+                DBServi.SaveChanges();
+                return "Se eliminó el Tipo Contrato correctamente";
+            }
+            catch (Exception ex)
+            {
+                return "No se pudo eliminar el Tipo Contrato: " + ex.Message;
+            }
+        }
+    }
+}
---
+++ App/Servicios/Servicios/Controllers/TipoContratosController.cs
@@ -0,0 +1,71 @@
+﻿using Servicios.Clases;
+using Servicios.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace Servicios.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    [RoutePrefix("api/TipoContratos")]
+    //[Authorize]
+    public class TipoContratosController : ApiController
+    {
+        [HttpGet] //Es el servicio que se va a exponer: GET, POST, PUT, DELETE
+        [Route("ConsultarTodos")] //Es el nombre de la funcionalidad que se va a ejecutar
+        public List<TipoContrato> ConsultarTodos()
+        {
+            //Se crea una instancia de la clase clsTipoContrato
+            clsTipoContrato TipoContrato = new clsTipoContrato();
+            //Se invoca el método ConsultarTodos() de la clase clsTipoContrato
+            return TipoContrato.ConsultarTodos();
+        }
+        [HttpGet]
+        [Route("ConsultarXIdTipoContrato")]
+        public TipoContrato ConsultarXIdTipoContrato(int IdTipoContrato)
+        {
+            //Se crea una instancia de la clases clsEmpleado
+            clsTipoContrato TipoContrato = new clsTipoContrato();
+            return TipoContrato.Consultar(IdTipoContrato);
+        }
+
+        [HttpPost]
+        [Route("Insertar")]
+        public string Insertar([FromBody] TipoContrato tipoContrato)
+        {
+            //Se crea una instancia de la clase clsEmpleado
+            clsTipoContrato TipoContrato = new clsTipoContrato();
+            //Se pasa la propieadad empleado al objeto de la clases clsEmpleado
+            TipoContrato.tipoContrato = tipoContrato;
+            //Se invoca el método insertar
+            return TipoContrato.Insertar();
+        }
+        [HttpPut]
+        [Route("Actualizar")]
+        public string Actualizar([FromBody] TipoContrato tipoContrato)
+        {
+            clsTipoContrato TipoContrato = new clsTipoContrato();
+            TipoContrato.tipoContrato = tipoContrato;
+            return TipoContrato.Actualizar();
+        }
+        [HttpDelete]
+        [Route("Eliminar")]
+        public string Eliminar([FromBody] TipoContrato tipoContrato)
+        {
+            clsTipoContrato TipoContrato = new clsTipoContrato();
+            TipoContrato.tipoContrato = tipoContrato;
+            return TipoContrato.Eliminar();
+        }
+        [HttpDelete]
+        [Route("EliminarXIdTipoContrato")]
+        public string EliminarXIdTipoContrato(int Documento)
+        {
+            clsTipoContrato TipoContrato = new clsTipoContrato();
+            return TipoContrato.EliminarXIdTipoContrato(Documento);
+        }
+    }
+}
